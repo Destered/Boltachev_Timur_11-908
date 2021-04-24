bb2dc5244366ff8dd715ac1da9a2fdb75afedd6c
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-04-24 18:21:54 +0300

    cookie

--- semestr-3sem/src/main/java/ru/destered/semestr3sem/security/token/TokenAuthenticationFilter.java
+++ semestr-3sem/src/main/java/ru/destered/semestr3sem/security/token/TokenAuthenticationFilter.java
@@ -10,23 +10,26 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        List<Cookie> list = Arrays.stream(request.getCookies()).filter(x->x.getName().equals("token")).collect(Collectors.toList());
+        List<Cookie> list = new ArrayList<>();
+        for(Cookie cookie : Arrays.asList(request.getCookies().clone())){
+            if(cookie.getName().equals("token")){
+                list.add(cookie);
+            }
+        }
 
         String token = null;
-
         if(!list.isEmpty()) {
             token = list.get(0).getValue();
         }
-
         if (token != null) {
             TokenAuthentication authentication = new TokenAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
